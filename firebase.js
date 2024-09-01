1ab9ced1796918ec84b3a6e6972a00c3cdd56002
Zhansar <daid8002@gmail.com>
2024-09-01 19:19:41 +0500

    changed config

--- firebase.js
+++ firebase.js
@@ -1,12 +1,5 @@
-import admin from 'firebase-admin';
-import serviceAccount from './akimat-bot-firebase-adminsdk-kf13w-832bf9a3d6.json' assert { type: 'json' };
+import { initializeApp } from "firebase/app";
+import config from "./config";
+const firebase = initializeApp(config.firebaseConfig);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://akimat-bot.appspot.com"
-});
-
-
-const bucket = admin.storage().bucket();
-
-export default bucket;
+export default firebase
--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const bot = new TelegramBot("7385522997:AAGTxQQ5wdYGF2fCVtC0cJo0PZxmkzNh_oE", {
       }    
 })
 
+// Handle /start command
+bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const username = msg.from.username;
+  
+    try {
+      
+    } catch (error) {
+      console.error('Error registering user:', error);
+      bot.sendMessage(chatId, 'Sorry, there was an error registering you. Please try again later.');
+    }
+  });
 
+  
 bot.on("polling_error", err => console.log(err.data.error.message));
 bot.on('photo', async img => {
     const file = bot.getFileStream(img.photo[img.photo.length-1].file_id);
@@ -25,3 +39,4 @@ bot.on('photo', async img => {
 bot.on('text', async msg => {
     await bot.sendMessage(msg.chat.id, msg.text);
 })
+
