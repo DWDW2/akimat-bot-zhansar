16838442f21e2eb48acda70d87391717450fd25c
Zhansar <daid8002@gmail.com>
2024-09-03 19:57:36 +0500

    pivot : changed firebase with supabase

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 // import {bucket} from "./firebase.js";
-import { bucket } from "./firebase.js";
+// import { bucket } from "./firebase.js";
 
 import fs from 'fs'
 import { createDocument } from "./db.js";
@@ -17,7 +17,7 @@ bot.onText(/\/start/, async (msg) => {
     const username = msg.from.username;
     console.log(msg)
     try {
-        createDocument('users', userId.toString(), {username:username})
+        createDocument('/users', userId.toString(), {username:username})
     } catch (error) {
       console.error('Error registering user:', error);
       bot.sendMessage(chatId, 'Sorry, there was an error registering you. Please try again later.');
@@ -25,20 +25,20 @@ bot.onText(/\/start/, async (msg) => {
   });
 
   
-bot.on("polling_error", err => console.log(err.data.error.message));
-bot.on('photo', async img => {
-    const file = bot.getFileStream(img.photo[img.photo.length-1].file_id);
-    const fileName = img.photo[img.photo.length-1].file_id
+// bot.on("polling_error", err => console.log(err.data.error.message));
+// bot.on('photo', async img => {
+//     const file = bot.getFileStream(img.photo[img.photo.length-1].file_id);
+//     const fileName = img.photo[img.photo.length-1].file_id
 
-    const uploadStream = bucket.file(fileName).createWriteStream({
-        contentType: 'image/jpeg'
-    });
-    file.pipe(uploadStream);
-    file.on('end', () => {
-        console.log('image recieved');
-    })
-})
-bot.on('text', async msg => {
-    await bot.sendMessage(msg.chat.id, msg.text);
-})
+//     const uploadStream = bucket.file(fileName).createWriteStream({
+//         contentType: 'image/jpeg'
+//     });
+//     file.pipe(uploadStream);
+//     file.on('end', () => {
+//         console.log('image recieved');
+//     })
+// })
+// bot.on('text', async msg => {
+//     await bot.sendMessage(msg.chat.id, msg.text);
+// })
 
